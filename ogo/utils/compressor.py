478d6bd182c64fca6d53efe542a4d2970b0b0797
Braden MacDonald <mail@bradenm.com>
2014-07-08 22:43:12 -0700

    Upgrade to Bootstrap 3.2.0

--- ogo/utils/compressor.py
+++ ogo/utils/compressor.py
@@ -12,15 +12,6 @@ class CssAbsoluteFilterFixed(CssAbsoluteFilter):
 
 
 class LessFilter(CompilerFilter):
-    """
-    CssAbsoluteFilter is sometimes not getting called when DEBUG=False
-    This affects any CSS files outside of the ogo/static/ directory,
-    resulting in missing images on the production site.
-    """
     def __init__(self, content, attrs, **kwargs):
-        cmd = settings.PROJECT_ROOT + "/node_modules/recess/bin/" + "recess --compile --compress {infile} > {outfile}"
+        cmd = settings.PROJECT_ROOT + "/node_modules/less/bin/lessc -x {infile} > {outfile}"
         super(LessFilter, self).__init__(content, command=cmd, **kwargs)
-
-    def input(self, **kwargs):
-        content = super(LessFilter, self).input(**kwargs)
-        return content  # CssAbsoluteFilterFixed(content).input(**kwargs)
