aaec3bcbed5d1e22c7c66c4abb578c353974fa0f
Braden MacDonald <mail@bradenm.com>
2016-02-23 20:38:34 -0800

    Add Car List plugin

--- ogo/plugins/cms_plugins.py
+++ ogo/plugins/cms_plugins.py
@@ -3,23 +3,67 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.contrib import admin
 from django.utils.translation import ugettext as _
+from ogo.utils import cse_api
 from .models import Partner, PageSection
 
 
 class CarMapPlugin(CMSPluginBase):
+    """
+    Plugin that displays a map of all the cars in the fleet.
+    """
     name = _("Map of all cars")
     render_template = "plugins/car_map.html"  # template to render the plugin with
 
     def render(self, context, instance, placeholder):
-        from ogo.utils import cse_api
         context['plugin_id'] = instance.pk
         fleet_data = cse_api.get_fleet()
+        all_car_ids = [car["id"] for loc in fleet_data["locations"].values() for car in loc['cars'].values()]
+        live_availability = cse_api.get_availability(all_car_ids)
         context['car_count'] = fleet_data["car_count"]
         context['locations'] = fleet_data["locations"].values()
         context['locations_count'] = len(fleet_data["locations"])
+        if live_availability:
+            for loc in fleet_data["locations"].values():
+                for car in loc['cars'].values():
+                    if car["id"] in live_availability:
+                        car["availability"] = live_availability[car["id"]]
         return context
 
 plugin_pool.register_plugin(CarMapPlugin)
+
+
+class CarListPlugin(CMSPluginBase):
+    name = _("List of all cars")
+    render_template = "plugins/car_list.html"  # template to render the plugin with
+
+    def render(self, context, instance, placeholder):
+        context['plugin_id'] = instance.pk
+        #car_details = {c.engage_id: c for c in VehicleDetails.objects.all()}
+        fleet_data = cse_api.get_fleet()
+        all_cars = []
+        for loc in fleet_data["locations"].values():
+            for car in loc['cars'].values():
+                car["location"] = loc
+                all_cars.append(car)
+                #if car["id"] in car_details:
+                #    car["description"] = car_details[car["id"]].description
+                #    car["image"] = car_details[car["id"]].image
+        all_cars = sorted(all_cars, key=lambda car: car["id"])
+        all_car_ids = [car["id"] for car in all_cars]
+        live_availability = cse_api.get_availability(all_car_ids)
+        context['car_count'] = fleet_data["car_count"]
+        context['cars'] = all_cars
+        context['locations'] = fleet_data["locations"].values()
+        context['locations_count'] = len(fleet_data["locations"])
+        if live_availability:
+            for car in all_cars:
+                if car["id"] in live_availability:
+                    car["availability"] = live_availability[car["id"]]
+            context["updated_time"] = live_availability["updated_at"]
+        return context
+
+plugin_pool.register_plugin(CarListPlugin)
+
 
 class PartnerPlugin(CMSPluginBase):
     model = Partner
