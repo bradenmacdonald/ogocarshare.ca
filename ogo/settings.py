8d3e9921b0b899995a9186e49765f1024429394e
Braden MacDonald <mail@bradenm.com>
2016-02-23 22:01:52 -0800

    Deprecate dev host

--- ogo/settings.py
+++ ogo/settings.py
@@ -61,7 +61,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['ogocarshare.ca', 'dev.ogocarshare.ca']
+ALLOWED_HOSTS = ['www.ogocarshare.ca', 'ogocarshare.ca']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
