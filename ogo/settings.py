f50e09d56cae589619a0aeaaa94becf592a184a2
Braden MacDonald <mail@bradenm.com>
2017-01-21 13:53:59 -0800

    Allow customizing static/media directories

--- ogo/settings.py
+++ ogo/settings.py
@@ -98,7 +98,7 @@ FIXTURE_DIRS = (os.path.join(OGO_ROOT, "fixtures"), )
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "user_media")
+MEDIA_ROOT = LOCAL_SETTINGS.get('MEDIA_ROOT', os.path.join(PROJECT_ROOT, "user_media"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -109,7 +109,7 @@ MEDIA_URL = "/media/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media")
+STATIC_ROOT = LOCAL_SETTINGS.get('STATIC_ROOT', os.path.join(PROJECT_ROOT, "site_media"))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
