7d464b0ea39c0af20e2efdef7e1dae3abf3f51dc
Braden MacDonald <mail@bradenm.com>
2017-01-21 13:29:10 -0800

    Add missing setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+Setup script for OGO Car Share Co-op Website
+"""
+from setuptools import setup
+
+setup(
+    name="OGO",
+    version="2.0.0",
+    install_requires=["setuptools"],
+    requires=[],
+    packages=["ogo"],
+)
